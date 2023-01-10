79a82385ad139c9d8332557b3acd4f6fccccff60
Jay Dickson <21349032+jaydickson02@users.noreply.github.com>
2023-01-10 15:45:26 +1100

    Speeds up the maze solve time

--- drawMaze.js
+++ drawMaze.js
@@ -17,52 +17,52 @@ var walls = [];
 var i = 0;
 
 
-function setup(){
+function setup() {
 
-//Auto Sizes so the maze fills the screen
-mazeDimensions.x = Math.floor(windowWidth/cellScale) - 2
+  //Auto Sizes so the maze fills the screen
+  mazeDimensions.x = Math.floor(windowWidth / cellScale) - 2
 
-mazeDimensions.y = Math.floor(windowHeight/cellScale) - 2
+  mazeDimensions.y = Math.floor(windowHeight / cellScale) - 2
 
-//Set framerate
-frameRate(1000);
+  //Set framerate
+  frameRate(1000);
 
-//Create canvas that can fit the maze
-createCanvas(mazeDimensions.x * cellScale + 1, mazeDimensions.y * cellScale + 1);
+  //Create canvas that can fit the maze
+  createCanvas(mazeDimensions.x * cellScale + 1, mazeDimensions.y * cellScale + 1);
 
-//Generate the maze cells
-filledArray = fillMazeCellArray(mazeDimensions);
+  //Generate the maze cells
+  filledArray = fillMazeCellArray(mazeDimensions);
 
-//Get a random cell to use as maze start point
-startRandomCell = getRandomCell(filledArray);
+  //Get a random cell to use as maze start point
+  startRandomCell = getRandomCell(filledArray);
 
-cellsInMaze = addToMaze(startRandomCell, cellsInMaze);
+  cellsInMaze = addToMaze(startRandomCell, cellsInMaze);
 
-//Generate frontier
-frontier = addFrontier(startRandomCell, cellsInMaze, frontier, mazeDimensions);
+  //Generate frontier
+  frontier = addFrontier(startRandomCell, cellsInMaze, frontier, mazeDimensions);
 
 
 }
 
 
-function draw(){
+function draw() {
 
   //Drawing everything
 
   //Draw the maze grid
-  for(var i = 0; i< filledArray.length; i++){
+  for (var i = 0; i < filledArray.length; i++) {
     fill(255);
     rect(filledArray[i].x * cellScale, filledArray[i].y * cellScale, cellScale, cellScale);
   }
 
   //Draw the maze cells
-  for(var i = 0; i < cellsInMaze.length; i++){
+  for (var i = 0; i < cellsInMaze.length; i++) {
     fill(255);
     rect(cellsInMaze[i].x * cellScale, cellsInMaze[i].y * cellScale, cellScale, cellScale);
   }
 
   //Draw the frontier
-  for(var i = 0; i < frontier.length; i++){
+  for (var i = 0; i < frontier.length; i++) {
     fill(200, 255, 200);
     rect(frontier[i].x * cellScale, frontier[i].y * cellScale, cellScale, cellScale);
   }
@@ -72,12 +72,12 @@ function draw(){
   rect(startRandomCell.x * cellScale, startRandomCell.y * cellScale, cellScale, cellScale);
 
   //Check that the wall array is not empty
-  if(walls.length > 0){
-  for(var i = 0; i < walls.length; i++){
-    stroke(255);
-    line(walls[i].x, walls[i].y, walls[i].x2, walls[i].y2);
-    stroke(0);
-  }
+  if (walls.length > 0) {
+    for (var i = 0; i < walls.length; i++) {
+      stroke(255);
+      line(walls[i].x, walls[i].y, walls[i].x2, walls[i].y2);
+      stroke(0);
+    }
   }
 
 
@@ -85,24 +85,25 @@ function draw(){
   //Logic starts here
 
   //Check frontier is not empty
+  for (let i = 0; i < 10; i++) { //Solves more of the maze per draw cycle
+    if (frontier.length > 0) {
 
-if(frontier.length > 0){
+      frontierRandomCell = getRandomCell(frontier);
 
-  frontierRandomCell = getRandomCell(frontier);
+      cellsInMaze = addToMaze(frontierRandomCell, cellsInMaze);
 
-  cellsInMaze = addToMaze(frontierRandomCell, cellsInMaze);
+      mazeNeighbours = checkForMazeNeighbours(frontierRandomCell, cellsInMaze);
 
-  mazeNeighbours = checkForMazeNeighbours(frontierRandomCell, cellsInMaze);
+      walls = removeWall(mazeNeighbours, frontierRandomCell, walls, cellScale);
 
-  walls = removeWall(mazeNeighbours, frontierRandomCell, walls, cellScale);
+      removeFromFrontier(frontierRandomCell, frontier);
 
-  removeFromFrontier(frontierRandomCell, frontier);
+      frontier = addFrontier(frontierRandomCell, cellsInMaze, frontier, mazeDimensions);
 
-  frontier = addFrontier(frontierRandomCell, cellsInMaze, frontier, mazeDimensions);
-
-} else {
-  console.log('Frontier Empty');
-}
+    } else {
+      console.log('Frontier Empty');
+    }
+  }
 
 
 }
