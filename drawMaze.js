6d4e78963452fdcab2a005688e9c319c16b3fca1
Jay Dickson <21349032+jaydickson02@users.noreply.github.com>
2023-01-10 15:42:07 +1100

    Quick Generate

--- drawMaze.js
+++ drawMaze.js
@@ -17,92 +17,64 @@ var walls = [];
 var i = 0;
 
 
-function setup(){
+function setup() {
 
-//Auto Sizes so the maze fills the screen
-mazeDimensions.x = Math.floor(windowWidth/cellScale) - 2
+  //Auto Sizes so the maze fills the screen
+  mazeDimensions.x = Math.floor(windowWidth / cellScale) - 2
 
-mazeDimensions.y = Math.floor(windowHeight/cellScale) - 2
+  mazeDimensions.y = Math.floor(windowHeight / cellScale) - 2
 
-//Set framerate
-frameRate(1000);
+  //Set framerate
+  frameRate(1);
 
-//Create canvas that can fit the maze
-createCanvas(mazeDimensions.x * cellScale + 1, mazeDimensions.y * cellScale + 1);
+  //Create canvas that can fit the maze
+  createCanvas(mazeDimensions.x * cellScale + 1, mazeDimensions.y * cellScale + 1);
 
-//Generate the maze cells
-filledArray = fillMazeCellArray(mazeDimensions);
+  //Generate the maze cells
+  filledArray = fillMazeCellArray(mazeDimensions);
 
-//Get a random cell to use as maze start point
-startRandomCell = getRandomCell(filledArray);
+  //Get a random cell to use as maze start point
+  startRandomCell = getRandomCell(filledArray);
 
-cellsInMaze = addToMaze(startRandomCell, cellsInMaze);
+  cellsInMaze = addToMaze(startRandomCell, cellsInMaze);
 
-//Generate frontier
-frontier = addFrontier(startRandomCell, cellsInMaze, frontier, mazeDimensions);
+  //Generate frontier
+  frontier = addFrontier(startRandomCell, cellsInMaze, frontier, mazeDimensions);
+
+  //Generate maze
+  while (frontier.length > 0) {
+
+    frontierRandomCell = getRandomCell(frontier);
+
+    cellsInMaze = addToMaze(frontierRandomCell, cellsInMaze);
+
+    mazeNeighbours = checkForMazeNeighbours(frontierRandomCell, cellsInMaze);
+
+    walls = removeWall(mazeNeighbours, frontierRandomCell, walls, cellScale);
+
+    removeFromFrontier(frontierRandomCell, frontier);
+
+    frontier = addFrontier(frontierRandomCell, cellsInMaze, frontier, mazeDimensions);
+
+  }
 
 
 }
 
 
-function draw(){
-
-  //Drawing everything
-
-  //Draw the maze grid
-  for(var i = 0; i< filledArray.length; i++){
-    fill(255);
-    rect(filledArray[i].x * cellScale, filledArray[i].y * cellScale, cellScale, cellScale);
-  }
+function draw() {
 
   //Draw the maze cells
-  for(var i = 0; i < cellsInMaze.length; i++){
+  for (var i = 0; i < cellsInMaze.length; i++) {
     fill(255);
     rect(cellsInMaze[i].x * cellScale, cellsInMaze[i].y * cellScale, cellScale, cellScale);
   }
 
-  //Draw the frontier
-  for(var i = 0; i < frontier.length; i++){
-    fill(200, 255, 200);
-    rect(frontier[i].x * cellScale, frontier[i].y * cellScale, cellScale, cellScale);
-  }
-
-  //Draw the starter cell
-  fill(200, 150, 150);
-  rect(startRandomCell.x * cellScale, startRandomCell.y * cellScale, cellScale, cellScale);
-
-  //Check that the wall array is not empty
-  if(walls.length > 0){
-  for(var i = 0; i < walls.length; i++){
+  //Draw the walls
+  for (var i = 0; i < walls.length; i++) {
     stroke(255);
     line(walls[i].x, walls[i].y, walls[i].x2, walls[i].y2);
     stroke(0);
   }
-  }
-
-
-
-  //Logic starts here
-
-  //Check frontier is not empty
-
-if(frontier.length > 0){
-
-  frontierRandomCell = getRandomCell(frontier);
-
-  cellsInMaze = addToMaze(frontierRandomCell, cellsInMaze);
-
-  mazeNeighbours = checkForMazeNeighbours(frontierRandomCell, cellsInMaze);
-
-  walls = removeWall(mazeNeighbours, frontierRandomCell, walls, cellScale);
-
-  removeFromFrontier(frontierRandomCell, frontier);
-
-  frontier = addFrontier(frontierRandomCell, cellsInMaze, frontier, mazeDimensions);
-
-} else {
-  console.log('Frontier Empty');
-}
-
 
 }
